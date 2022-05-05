526ba55a5396d076da4d068d29f4f592ee2544e5
Kasidit Bualoeng <subdit@gmail.com>
2022-05-05 14:13:31 -0400

    create AddItem.js file

---
+++ grocery-list/shopping-list/src/AddItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaPlus } from 'react-icons/fa';
+import { useRef } from 'react';
+
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  // use to focus the cursor back on the input where we want it to focus on.
+  const inputRef = useRef();
+  return (
+    <form className='addForm' onSubmit={handleSubmit}>
+      <label htmlFor='addItem'>Add Item</label>
+      <input
+        autoFocus
+        ref={inputRef}
+        id='addItem'
+        type='text'
+        placeholder='Add your item'
+        required
+        value={newItem} // Put the item in the state
+        onChange={e => setNewItem(e.target.value)} // onChange is to set a new item set in the value={newItem}
+      ></input>
+      <button
+        type='submit'
+        aria-label='Add Item'
+        onClick={() => inputRef.current.focus()}>
+        <FaPlus />
+      </button>
+    </form>
+  );
+};
+export default AddItem;
--- grocery-list/shopping-list/src/App.js
+++ grocery-list/shopping-list/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './Header';
 import SearchItem from './SearchItem';
 import AddItem from './AddItem';
