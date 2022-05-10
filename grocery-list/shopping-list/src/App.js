7d17de254fe732bc9607c37806b4308a567c515e
Kasidit Bualoeng <subdit@gmail.com>
2022-05-10 19:04:04 -0400

    add isLoading function and add logic if the state is loading by set timeOut to show the message

--- grocery-list/shopping-list/src/App.js
+++ grocery-list/shopping-list/src/App.js
@@ -7,15 +7,45 @@ import Footer from './Footer';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('shoppinglist')) || []
-  );
+  const API_URL = 'http://localhost:3500/items';
+
+  const [items, setItems] = useState([]);
+  // const [items, setItems] = useState(
+  //   JSON.parse(localStorage.getItem('shoppinglist')) || []
+  // );
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
+  // using asynch await not fetch.then inside useEffect to async and await for the API_URL to response
+  // use try {} catch(err) and (async () => await fetchItems())();
   useEffect(() => {
-    localStorage.setItem('shopping list', JSON.stringify(items));
-  }, [items]);
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data'); // set fetchError to catch the response message
+
+        const listItems = await response.json();
+        console.log(listItems);
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        // console.log(err.message);
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    // set time out for loading 2 second
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 2000);
+  }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem('shopping list', JSON.stringify(items));
+  // }, [items]);
 
   const addItem = item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -52,13 +82,20 @@ function App() {
         handleSubmit={handleSubmit}
       />
       <SearchItem search={search} setSearch={setSearch} />
-      <Content
-        items={items.filter(item =>
-          item.item.toLowerCase().includes(search.toLowerCase())
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter(item =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
         )}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      </main>
+
       <Footer length={items.length} />
     </div>
   );
